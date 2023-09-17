ee56ed3f3201aa78b2832018c1c3c590aace64da
Amirkaramat <amir_karamat@yahoo.com>
2023-09-17 13:04:51 +0330

    update navbar-actions.tsx

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Button from '@/components/ui/button'
+"use client";
+import { useEffect, useState } from "react";
+import Button from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
 
 const NavbarActions = () => {
-  return (
-    <div className='ml-auto flex items-center gap-x-4'>
-        <Button></Button>
-    </div>
-  )
-}
+  const [isMounted, setIsMounted] = useState(false);
 
-export default NavbarActions
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2 ">
+        <ShoppingBag size={20} color="white" />
+        <span className="text-sm ml-2 font-medium text-white">0</span>
+      </Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
