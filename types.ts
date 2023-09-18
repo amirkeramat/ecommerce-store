952c72b7e4874515ea0ef3244109ea9a9874b165
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 14:58:03 +0330

    Update types.ts
    add color,size,image,product interfaces

--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 export interface Billboard {
   id: string;
-  name: string;
+  label: string;
   imageUrl: string;
 }
 
@@ -8,4 +8,32 @@ export interface Category {
   id: string;
   name: string;
   billboard: Billboard;
+}
+
+export interface Color {
+  id: string;
+  name:string;
+  value:string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  size: Size;
+  color: Color;
+  images: Image[];
+  isFeatured: boolean;
 }
