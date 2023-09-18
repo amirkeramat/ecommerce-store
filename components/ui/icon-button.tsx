a578fb8b9bd823dad81e5a58c173f9e2ecdbe09c
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 15:14:37 +0330

    Create icon-button.tsx

---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import React, { MouseEventHandler } from "react";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  icon: React.ReactElement;
+  className?: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  onClick,
+  icon,
+  className,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "flex items-center rounded-full justify-center bg-white border shadow-md p-2 hover:scale-110 transition ",
+        className
+      )}>
+        {icon}
+      </button>
+  );
+};
+
+export default IconButton;
