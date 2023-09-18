8ce6e01146bee91843825357ad3d895359b860ea
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 14:59:09 +0330

    Update getCategories.ts
    add axios instead of fetch

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -5,7 +5,7 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
   const res = await axios.get(URL);
-  return res.data;
+  return res.data
 };
 
 export default getCategories;
