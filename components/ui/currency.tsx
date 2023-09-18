caf7dc085f84cc1c2c5b090ed70b88e9e78733a6
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 15:15:31 +0330

    Create currency.tsx

---
+++ components/ui/currency.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import { formatter } from "@/lib/utils";
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({ value }) => {
+    
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <div className="font-semibold">{formatter.format(Number(value))}</div>;
+};
+
+export default Currency;
