2a4d5b23da5bc483bd88dc91f1ae0170add5784c
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 15:17:16 +0330

    Create page.tsx
    create product folder the [productId] folder then page.tsx in it

---
+++ app/(routes)/product/[productId]/page.tsx
@@ -0,0 +1,43 @@
+interface ProductPageProps {
+  params: {
+    productId: string;
+  };
+}
+
+import getProduct from "@/actions/get-product";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+import Gallery from "@/components/gallery";
+import React from "react";
+import Info from "@/components/info";
+
+const ProductPage: React.FC<ProductPageProps> = async ({ params: { productId } }) => {
+
+    const product =await getProduct(productId)
+
+    const suggestedProducts = await getProducts({
+        categoryId:product?.category?.id
+    })
+
+    console.log(suggestedProducts);
+    
+
+
+  return <div className="bg-white">
+    <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+            <div className="md:grid md:grid-cols-2 lg:items-start md:gap-x-4">
+                <Gallery images={product.images}/>
+                <div className="mt-10 px-4 md:mt-0 sm:px-0 ">
+                    <Info data={product}/>
+                </div>
+            </div>
+            <hr className="my-10"/>
+            <ProductList title="Related items" items={suggestedProducts} />
+        </div>
+    </Container>
+  </div>;
+};
+
+export default ProductPage;
