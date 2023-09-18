9ae0781ad274d6e211ed9da7e8817890a4033be4
Amirkaramat <amir_karamat@yahoo.com>
2023-09-18 15:12:38 +0330

    Update page.tsx (homepage)

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("db622121-c697-4da0-8b8a-e20b73c3706b");
+  const products = await getProducts({ isFeatured: true });
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
