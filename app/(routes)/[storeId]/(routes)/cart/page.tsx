5be787aad851308ec43e22a274745a9149a694d7
Amirkaramat <amir_karamat@yahoo.com>
2023-09-24 11:18:04 +0330

    fix cart page

--- app/(routes)/[storeId]/(routes)/cart/page.tsx
+++ app/(routes)/[storeId]/(routes)/cart/page.tsx
@@ -20,7 +20,7 @@ const CartPage = () => {
   return (
     <div className="">
       <Container>
-        <div className="px-4 h-[85vh] py-16 sm:px-6 lg:px-8">
+        <div className="px-4  py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
